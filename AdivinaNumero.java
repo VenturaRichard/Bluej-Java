b24d9ba8c38e8e56f29d41ce609e7243666d147e
Venturarichard <venturarichard87@gmail.com>
2023-09-30 03:13:13 -0300

    Practica de adivina el numero

--- AdivinaNumero.java
+++ AdivinaNumero.java
@@ -9,12 +9,15 @@ public class AdivinaNumero
    int numeroSecreto;
    int intentos;
    boolean adivinado;
-   
+   int maxIntentos;
+   String jugadorConMasIntentos;
    public AdivinaNumero(){
     Random random=new Random();
     this.numeroSecreto=random.nextInt(100)+1;
     this.intentos=0;
     this.adivinado=false;
+    this.maxIntentos=0;
+    this.jugadorConMasIntentos="";
     }
     
     public void jugar(){
@@ -23,8 +26,10 @@ public class AdivinaNumero
         System.out.println("Estoy pensando en un número entre 1 y 100.");
         
       while(adivinado==false){
-        int intento = scanner.nextInt();
         intentos++;
+        int intento = scanner.nextInt();
+        if(intentos<=20){
+         System.out.println("Intentos:"+intentos);
         if(intento==numeroSecreto){
         System.out.println("Acerto al numero");
         adivinado=true;
@@ -38,8 +43,28 @@ public class AdivinaNumero
             adivinado=false;
             }
         }
+        }else{
+        System.out.println("Se acabo los intentos");
         }
+        
+      }
           scanner.close();
         }
+    public void intentoMax(AdivinaNumero adivinaNumero){
+        if(intentos>=adivinaNumero.intentos){
+        System.out.println("jugador con intentos"+intentos);
+        }else{
+        System.out.println("Empate"+intentos);
+        }
+        
+    }
+        public static void main(String[] args) {
+        AdivinaNumero juego = new AdivinaNumero();
+        AdivinaNumero juego1 = new AdivinaNumero();
+        juego.jugar();
+        System.out.println(juego.jugadorConMasIntentos + " realizó la mayor cantidad de intentos: " + juego.maxIntentos);
+        juego1.jugar();
+        System.out.println(juego.jugadorConMasIntentos + " realizó la mayor cantidad de intentos: " + juego.maxIntentos);
+    }
     }
 
