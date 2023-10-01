0bc3ae26a71f761bc82604f564aaf86b097620bd
Richard <venturarichard87@gmail.com>
2023-10-01 02:46:04 -0400

    Piedra, papel y tijeras

--- JuegoPiedraPapelTijeras.java
+++ JuegoPiedraPapelTijeras.java
@@ -1,33 +1,72 @@
 
-/**
- * Write a description of class JuegoPiedraPapelTijeras here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
+import java.util.Random;
+import java.util.Scanner;
 public class JuegoPiedraPapelTijeras
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class JuegoPiedraPapelTijeras
-     */
-    public JuegoPiedraPapelTijeras()
-    {
-        // initialise instance variables
-        x = 0;
+    private int puntajeJugador;
+    private int puntajeComputadora;
+    
+    public JuegoPiedraPapelTijeras(){
+    this.puntajeJugador=0;
+    this.puntajeComputadora=0;
     }
+    
+    public void Jugar(){
+    Scanner scanner= new Scanner(System.in);
+    Random randon =new Random();
+    System.out.println("Bienvenido ");
+    System.out.println("Puedes elegir: Piedra, Papel o Tijeras. Para salir, escribe 'Salir'." );
+    
+    while(true){
+    System.out.println("Elige tu jugada ");
+    String eleccionJugador=scanner.nextLine().toLowerCase();
+    if(eleccionJugador.equals("salir")){
+     break;
+    }
+    if (!eleccionJugador.equals("piedra") && !eleccionJugador.equals("papel") && !eleccionJugador.equals("tijeras")) {
+     System.out.println("Elección no válida. Por favor, elige Piedra, Papel o Tijeras.");
+     continue;
+            }
+    
+      int numeroAleatorio=randon.nextInt(3);
+      String eleccionComputadora=obtenerEleccionComputadora(numeroAleatorio);
+      System.out.println("La computadora elijio : "+ eleccionComputadora);
+      String resultado = determinarGanador(eleccionJugador, eleccionComputadora);
+      System.out.println(resultado);
+       
+      if(resultado.equals("Ganaste")){
+        puntajeJugador++;
+        }else if(resultado.equals("La computadora gano")){
+        puntajeComputadora++;
+        }
+      System.out.println("Puntaje actual - Jugador: " + puntajeJugador + " | Computadora: " + puntajeComputadora);
+           }
 
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+        System.out.println("Gracias por jugar.");
+        scanner.close();
+    }
+ 
+  public String obtenerEleccionComputadora(int numeroAleatorio){
+    switch(numeroAleatorio){
+    case 0:
+        return "Piedra";
+    case 1:
+        return "Papel";
+    case 3:
+        return "Tijera";
+        default:
+            return "";
+    }
+    }
+    public String determinarGanador(String eleccionJugador, String eleccionComputadora){
+    if(eleccionJugador.equals(eleccionComputadora)){
+    return "Empate";
+    }else if ((eleccionJugador.equals("piedra") && eleccionComputadora.equals("Tijeras"))
+                || (eleccionJugador.equals("papel") && eleccionComputadora.equals("Piedra"))
+                || (eleccionJugador.equals("tijeras") && eleccionComputadora.equals("Papel"))) {
+            return "Ganaste";
+    }else{
+    return "La computadora gano";
+    }
     }
 }
